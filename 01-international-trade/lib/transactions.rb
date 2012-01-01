6514af2b66dffa05a584a970f87c6c14af5f6a07
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-01-01 00:33:19 -0800

    Add reduce_for_sku method and drop [] method.

--- 01-international-trade/lib/transactions.rb
+++ 01-international-trade/lib/transactions.rb
@@ -1,15 +1,29 @@
 require "csv"
+require "bigdecimal"
 
 class Transactions
-  attr_reader :data
+  attr_reader :data, :conversions
 
-  def initialize(filename)
+  def initialize(filename, conversions)
     @data = CSV.read("fixtures/#{filename}", headers: true, header_converters: :symbol)
+    @conversions = conversions
   end
 
-  def [](index)
-    row = @data[index]
-    amount, currency = row[:amount].split
-    { store: row[:store], sku: row[:sku], amount: amount, currency: currency }
+  def reduce_for_sku(sku)
+    total = BigDecimal("0.00")
+
+    @data.each do |row|
+      next unless row[:sku] == sku
+
+      amount, currency = row[:amount].split
+
+      if currency == "USD"
+        total += BigDecimal(amount)
+      else
+        total += conversions.convert(amount, currency, "USD")
+      end
+    end
+
+    total.to_s("f")
   end
 end
--- 01-international-trade/spec/transactions_spec.rb
+++ 01-international-trade/spec/transactions_spec.rb
@@ -2,16 +2,10 @@ require_relative "../lib/transactions.rb"
 
 describe Transactions do
   before do
-    @transactions = Transactions.new("SAMPLE_TRANS.csv")
+    @transactions = Transactions.new("SAMPLE_TRANS.csv", mock("Conversions"))
   end
 
   it "reads in data from a CSV file" do
     @transactions.data.class.should == CSV::Table
-  end
-
-  describe "#[]" do
-    it "returns a hash with details of a transaction" do
-      @transactions[0].should == { store: "Yonkers", sku: "DM1210", amount: "70.00", currency: "USD" }
-    end
   end
 end
