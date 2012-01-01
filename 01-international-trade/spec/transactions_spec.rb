cd80e7d72b2ddd861b5e364b80a2f150f6d838ed
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-01-01 00:39:40 -0800

    Add spec for reduce_for_sku and use real files from executable.

--- 01-international-trade/spec/transactions_spec.rb
+++ 01-international-trade/spec/transactions_spec.rb
@@ -2,10 +2,17 @@ require_relative "../lib/transactions.rb"
 
 describe Transactions do
   before do
-    @transactions = Transactions.new("SAMPLE_TRANS.csv", mock("Conversions"))
+    conversions = Conversions.new("SAMPLE_RATES.xml")
+    @transactions = Transactions.new("SAMPLE_TRANS.csv", conversions)
   end
 
   it "reads in data from a CSV file" do
     @transactions.data.class.should == CSV::Table
+  end
+
+  describe "#reduce_for_sku" do
+    it "finds the total sales for the given SKU in USD" do
+      @transactions.reduce_for_sku("DM1182").should == "134.22"
+    end
   end
 end
