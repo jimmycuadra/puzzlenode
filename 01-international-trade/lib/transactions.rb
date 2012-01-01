063cef84d2021cbdbb6b289f16a8c7cb35997018
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-01-01 00:06:01 -0800

    Add [] method to Transactions.

--- 01-international-trade/lib/transactions.rb
+++ 01-international-trade/lib/transactions.rb
@@ -6,4 +6,10 @@ class Transactions
   def initialize(filename)
     @data = CSV.read("fixtures/#{filename}", headers: true, header_converters: :symbol)
   end
+
+  def [](index)
+    row = @data[index]
+    amount, currency = row[:amount].split
+    { store: row[:store], sku: row[:sku], amount: amount, currency: currency }
+  end
 end
--- 01-international-trade/spec/transactions_spec.rb
+++ 01-international-trade/spec/transactions_spec.rb
@@ -8,4 +8,10 @@ describe Transactions do
   it "reads in data from a CSV file" do
     @transactions.data.class.should == CSV::Table
   end
+
+  describe "#[]" do
+    it "returns a hash with details of a transaction" do
+      @transactions[0].should == { store: "Yonkers", sku: "DM1210", amount: "70.00", currency: "USD" }
+    end
+  end
 end
